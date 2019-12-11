1c8d6f6fe2ce4e5a87d69db2a87c3d9178eb67cb
Arvida Jimenez <arvida.jimenez@gmail.com>
2019-12-11 21:27:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ let elementMain = document.querySelector("main#main");
 elementMain.remove();
 
 
-let newHeader = document.createElement("h1#victory");
+var newHeader = document.createElement("h1#victory");
