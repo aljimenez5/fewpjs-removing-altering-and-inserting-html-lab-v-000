cf706c9989dd825d927fff07ed6a98af516a59e1
Arvida Jimenez <arvida.jimenez@gmail.com>
2019-12-11 21:32:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ elementMain.remove();
 
 var newHeader = document.createElement("h1");
 newHeader.setAttribute("id", "victory");
-newHeader.setAttribute("nodeName", "Arv is the champion");
+newHeader.setAttribute("", "Arv is the champion");
